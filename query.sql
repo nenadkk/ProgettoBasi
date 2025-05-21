46ddc6cd61007f611645e62db24a9927d3bdc0db
nenad-radulovic <nenad.radulovic@studenti.unipd.it>
2025-05-21 14:57:05 +0200

    Sistemati gli errori delle query di creazione

--- query.sql
+++ query.sql
@@ -1,90 +1,93 @@
+--tutti i nomi di tabelle sono con l'iniziale maiuscole e il resto minuscolo
+--tutti gli attributi sono in minuscolo 
+
 CREATE TABLE Persona(
     codice_fiscale CHAR(16) PRIMARY KEY,
-    nome VARCHAR NOT NULL,
-    cognome VARCHAR NOT NULL
-)
+    nome VARCHAR(100) NOT NULL,
+    cognome VARCHAR(100) NOT NULL
+);
 
 CREATE TABLE Certificatore(
     partita_iva CHAR(11) PRIMARY KEY,
-    nome VARCHAR NOT NULL,
-    sede VARCHAR NOT NULL,
-    anno_fondazione YEAR NOT NULL,
+    nome VARCHAR(100) NOT NULL,
+    sede VARCHAR(100) NOT NULL,
+    anno_fondazione SMALLINT NOT NULL,
     collaboratori SMALLINT NOT NULL,
-    tipologia VARCHAR NOT NULL,
+    tipologia VARCHAR(100) NOT NULL,
 
     CHECK (anno_fondazione>1000),
     CHECK (collaboratori>0),
     CHECK (partita_iva ~ '^[0-9]{11}$')
-)
+);
 
 CREATE TABLE Banditore(
     partita_iva CHAR(11) PRIMARY KEY,
-    nome VARCHAR NOT NULL,
-    sede VARCHAR NOT NULL,
-    anno_fondazione YEAR NOT NULL,
+    nome VARCHAR(100) NOT NULL,
+    sede VARCHAR(100) NOT NULL,
+    anno_fondazione SMALLINT NOT NULL,
     collaboratori SMALLINT NOT NULL,
     esperienza SMALLINT NOT NULL,
 
     CHECK (anno_fondazione>1000),
     CHECK (collaboratori>0),
     CHECK (partita_iva ~ '^[0-9]{11}$')
-)
+);
 
 CREATE TABLE Sponsor(
     partita_iva CHAR(11) PRIMARY KEY,
-    nome VARCHAR NOT NULL,
-    sede VARCHAR NOT NULL,
-    anno_fondazione YEAR NOT NULL,
+    nome VARCHAR(100) NOT NULL,
+    sede VARCHAR(100) NOT NULL,
+    anno_fondazione SMALLINT NOT NULL,
     collaboratori SMALLINT NOT NULL,
-    tipologia VARCHAR NOT NULL,
-    livello VARCHAR NOT NULL,
+    tipologia VARCHAR(100) NOT NULL,
+    livello VARCHAR(100) NOT NULL,
 
     CHECK (anno_fondazione>1000),
     CHECK (collaboratori>0),
     CHECK (partita_iva ~ '^[0-9]{11}$')
-)
+);
 
 CREATE TABLE Prodotto(
     codice CHAR(12) PRIMARY KEY,
-    nome VARCHAR NOT NULL,
+    nome VARCHAR(100) NOT NULL,
     valutazione INT NOT NULL,
-    tipologia VARCHAR NOT NULL,
+    tipologia VARCHAR(100) NOT NULL,
     proprietario CHAR(16),
 
     CHECK (valutazione>0), --un prodotto non può avere valutazione negativa
     CHECK (codice ~ '^[0-9]{12}$'),
 
     FOREIGN KEY(proprietario) REFERENCES Persona(codice_fiscale)
-)
+);
 
 CREATE TABLE Certificato(
     codice CHAR(12) NOT NULL,
     certificatore CHAR(11),
-    nome VARCHAR NOT NULL,
+    nome VARCHAR(100) NOT NULL,
     prodotto CHAR(12),
     data_emanazione DATE NOT NULL,
-    tecnica_analisi VARCHAR NOT NULL,
+    tecnica_analisi VARCHAR(100) NOT NULL,
 
     CHECK (codice ~ '^[0-9]{12}$'),
     FOREIGN KEY(certificatore) REFERENCES Certificatore(partita_iva),
     FOREIGN KEY(prodotto) REFERENCES Prodotto(codice),
     CONSTRAINT PK_Certificato PRIMARY KEY(codice,certificatore)
-)
+);
 
 CREATE TABLE Specializzazione(
     codice CHAR(12) NOT NULL,
     certificatore CHAR(11),
-    nome VARCHAR NOT NULL,
+    nome VARCHAR(100) NOT NULL,
     banditore CHAR(11),
     data_emanazione DATE NOT NULL,
-    livello VARCHAR NOT NULL,
+    livello VARCHAR(100) NOT NULL,
     
     CHECK (codice ~ '^[0-9]{12}$'),
     FOREIGN KEY(certificatore) REFERENCES Certificatore(partita_iva),
     FOREIGN KEY(banditore) REFERENCES Banditore(partita_iva),
 
     CONSTRAINT PK_Specializzazione PRIMARY KEY(codice,certificatore)
-)
+);
 
 CREATE TABLE Asta(
     id_asta CHAR(12) PRIMARY KEY,
@@ -101,7 +104,7 @@ CREATE TABLE Asta(
     FOREIGN KEY(prodotto) REFERENCES Prodotto(codice),
     FOREIGN KEY(banditore) REFERENCES Banditore(partita_iva),
     FOREIGN KEY(sponsor) REFERENCES Sponsor(partita_iva)
-)
+);
 
 CREATE TABLE Offerta(
     codice CHAR(12) PRIMARY KEY,
@@ -113,7 +116,7 @@ CREATE TABLE Offerta(
     CHECK (codice ~ '^[0-9]{12}'),
     FOREIGN KEY(asta) REFERENCES Asta(id_asta),
     FOREIGN KEY(offerente) REFERENCES Persona(codice_fiscale)
-)
+);
 
 
 
